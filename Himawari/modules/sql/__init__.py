cc069e8ee1dfb92780b26a05a020535ad5c9d117
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-07-29 14:00:55 +0530

    Update __init__.py

--- Himawari/modules/sql/__init__.py
+++ Himawari/modules/sql/__init__.py
@@ -25,7 +25,7 @@ SOFTWARE.
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from Himawari import DB_URL
+from Himawari import DB_URI
 
 
 
